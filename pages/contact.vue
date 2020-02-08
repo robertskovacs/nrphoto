08883a8566fbff6cd0f87355eb1e2bc9145e8ac4
Robert Kovacs <roberts.kovacs@gmail.com>
2020-02-08 14:21:42 +0100

    Contact form updated

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,10 +5,10 @@
             </div>
             <div class="column is-one-third">
                 <form name="contact" method="POST" data-netlify="true">  
-                    <input class="input" type="text" placeholder="Név (kötelező)">
-                    <input class="input" type="text" placeholder="E-mail cím (kötelező)">
-                    <input class="input" type="tel" placeholder="Telefonszám">
-                    <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+                    <input class="input" type="text" name="name" placeholder="Név (kötelező)">
+                    <input class="input" type="text" name="email" placeholder="E-mail cím (kötelező)">
+                    <input class="input" type="tel" name="telephone" placeholder="Telefonszám">
+                    <textarea class="textarea" name="message" placeholder=""></textarea>
                     <div class="control">
                         <button class="button is-primary" type="submit">Submit</button>
                     </div>
