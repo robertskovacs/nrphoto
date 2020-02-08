b6386f1fa904012f99f1a38c40335b6dc16653b8
Robert Kovacs <roberts.kovacs@gmail.com>
2020-02-08 14:40:48 +0100

    Navbar added

---
+++ components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="navigation">
+        
+        <nav class="navbar" v-bind:class="{ hidden: !isOpen}" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+
+            <a class="brand-item" href="/">
+              Nagy Richárd Photo
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="isOpen = !isOpen" v-bind:class="{'is-active': isOpen}">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
+            <div class="navbar-end">
+              <a href="/contact" class="navbar-item contact-link">
+                Contact
+              </a>
+
+            </div>
+
+          </div>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: function() {
+    return {
+        isOpen: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.logo {
+  height:50px;
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="contact">
+    <div class="container">
+        <Navbar />
         <div class="columns">
             <div class="column">
             </div>
@@ -8,9 +9,9 @@
                     <input class="input" type="text" name="name" placeholder="Név (kötelező)">
                     <input class="input" type="text" name="email" placeholder="E-mail cím (kötelező)">
                     <input class="input" type="tel" name="telephone" placeholder="Telefonszám">
-                    <textarea class="textarea" name="message" placeholder=""></textarea>
+                    <textarea class="textarea" name="message" placeholder="Kérlek írj magadról és az elképzeléseidről pár sort."></textarea>
                     <div class="control">
-                        <button class="button is-primary" type="submit">Submit</button>
+                        <button class="button is-primary" type="submit">Küldés</button>
                     </div>
                     
                 </form>
@@ -20,3 +21,12 @@
         </div>
     </div>
 </template>
+
+<script>
+import Navbar from "~/components/Navbar"
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
